c15c48e8a01be94e7a2e28cb227301fd7cf76f78
egleves <egleves@tlu.ee>
2024-09-22 17:35:57 +0300

    feat: added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MyButton from './components/MyButton';
-import MyField from './components/MyField';
-import MyText from './components/MyText';
-
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MyButton from "./components/MyButton";
+import MyField from "./components/MyField";
+import MyText from "./components/MyText";
 
 function App() {
   return (
     <>
-    <MyButton/>
-    <MyField/>
+      <MyButton />
+      <MyField />
     </>
   );
 }
